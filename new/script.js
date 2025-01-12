732813a0d59e0ae44eaf801a9c724da20f838911
Divyansh pathak <divyanshpathak@Divyanshs-MacBook-Air.local>
2025-01-12 19:32:12 +0530

    Your commit message

---
+++ new/script.js
@@ -0,0 +1,23 @@
+let score = prompt("enter you score (0-100)")
+let grade;
+
+
+if(score >= 90 && score <=100){
+    grade = 'A';
+} 
+
+else if(score >=70 && score <= 89){
+    grade = 'B';
+}
+
+else if(score >= 60 && score <= 69){
+    grade = 'C';
+}
+
+else if(score >= 50 && score <= 59){
+    grade = 'D';
+}
+else if(score >= 0 && score <= 49){
+    grade = 'F';
+}
+console.log("according to your score, you grade was : ",grade)
